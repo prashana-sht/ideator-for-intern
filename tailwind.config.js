694f15299651b7a9d4a3caa07591052f7d5a5263
prashana-sht <shrestha.prashana@gmail.com>
2025-07-11 13:59:31 +0545

    idea

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  theme: {
+    'fontFamily': {
+      'open-sans': ['Open Sans', 'sans-serif'],
+      'dosis': ['Dosis', 'sans-serif'],
+      'pacifico': ['Pacifico', 'sans-serif']
+    },
+    'colors': {
+      ...colors,
+      'primary': {
+        500: '#337ab7'
+      }
+    }
+  }
+}
